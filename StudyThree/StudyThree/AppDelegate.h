ba3482c46e0e719e3df07cdb09b56ea65996d0d5
fukai <fukai@xcloud.cc>
2016-11-21 14:45:26 +0800

    提交 studyThree

---
+++ StudyThree/StudyThree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudyThree
+//
+//  Created by 付凯 on 2016/11/21.
+//  Copyright © 2016年 fukai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StudyThree/StudyThree/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StudyThree
+//
+//  Created by 付凯 on 2016/11/21.
+//  Copyright © 2016年 fukai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
