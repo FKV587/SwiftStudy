6dd5cf1d6b0befccbc77d9c249aede7acc2ea4be
fukai <fukai@xcloud.cc>
2016-11-23 14:52:47 +0800

    同上 毛玻璃效果 秀秀秀秀

--- StudyFive/StudyFive/ViewController.swift
+++ StudyFive/StudyFive/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var icon_image: UIImageView!
+    @IBOutlet weak var visualeffectView: UIVisualEffectView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if #available(iOS 10.0, *) {
+            let blurEffect = UIBlurEffect.init(style: UIBlurEffectStyle.regular)
+            visualeffectView.effect = blurEffect
+        } else {
+            // Fallback on earlier versions
+            let blurEffect = UIBlurEffect.init(style: UIBlurEffectStyle.light)
+            visualeffectView.effect = blurEffect
+        }
+        self.icon_image.addSubview(visualeffectView)
     }
 
     override func didReceiveMemoryWarning() {
